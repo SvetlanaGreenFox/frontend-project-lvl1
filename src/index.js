2ffdf764ba54bf6626ee3b009360879d603dfbc7
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-08-27 07:55:34 +0300

    Changed the couter to for

--- src/index.js
+++ src/index.js
@@ -7,37 +7,31 @@ export const getRndInteger = (min, max) => {
 
 const checkAnswer = (answer, expectedAnswer) => answer === expectedAnswer;
 
-const askQuestion = (func, userName, count) => {
-  count += 1;
-  const expression = func();
-  console.log(expression);
-  const output = expression[0];
-  const correctAnswer = expression[1];
-  const answer = readlineSync.question(`Question: ${output}\nYour answer: `);
-  const check = checkAnswer(correctAnswer, answer);
-
-  if (check) {
-    console.log('Correct!');
-    if (count === 3) {
-      console.log(`Congratulations, ${userName}!`);
-    } else {
-      askQuestion(func, userName, count);
-    }
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-  }
-};
-
 export const madeGame = (taskDescription, makeTask) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(taskDescription);
-  const count = 0;
 
-  askQuestion(makeTask, userName, count);
+  const countRounds = 3;
+
+  for (let i = 0; i < countRounds; i += 1) {
+    const expression = makeTask();
+    console.log(expression);
+    const output = expression[0];
+    const correctAnswer = expression[1];
+    const answer = readlineSync.question(`Question: ${output}\nYour answer: `);
+    const check = checkAnswer(correctAnswer, answer);
+
+    if (check) {
+      console.log('Correct!');
+      if (i === 2) {
+        console.log(`Congratulations, ${userName}!`);
+        break;
+      }
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+  }
 };
-
-for (let count = 0; count < 3; count += 1) {
-  askQuestion
-}
