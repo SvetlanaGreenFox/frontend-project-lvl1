b0c8ef068ce59624d440aea42012f0a04dd42a53
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-09-01 22:10:58 +0300

    Deleted str

--- src/games/progression.js
+++ src/games/progression.js
@@ -16,7 +16,6 @@ const getProgression = (start, step, length) => {
 
 const getGameData = () => {
   const combinationNum = getProgression(3, getRndInteger(1, 5), getRndInteger(10, 15));
-
   const hiddenNumber = getRndInteger(0, combinationNum.length - 1);
   const correctAnswer = combinationNum[hiddenNumber].toString();
   combinationNum[hiddenNumber] = '..';
