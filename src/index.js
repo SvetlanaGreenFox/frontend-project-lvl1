983b64490d2ba9588d754a2213b565d5b8dd9920
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-09-01 13:37:20 +0300

    Relocated countRounds

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import readlineSync from 'readline-sync';
 
+const countRounds = 3;
+
 export const madeGame = (taskDescription, getGameData) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(taskDescription);
-
-  const countRounds = 3;
 
   for (let i = 0; i < countRounds; i += 1) {
     const gameData = getGameData();
@@ -19,11 +19,10 @@ export const madeGame = (taskDescription, getGameData) => {
       console.log('Correct!');
       if (i === 2) {
         console.log(`Congratulations, ${userName}!`);
-        break;
       }
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-      break;
+      return;
     }
   }
 };
